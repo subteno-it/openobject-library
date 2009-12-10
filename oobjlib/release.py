84d86e0a441d2e6e5382138d6fe56068a5b7a1fe
Christophe Chauvet <christophe.chauvet@syleam.fr>
2009-12-10 19:44:40 +0100

    [ADD] Add setup.py and release file

---
+++ oobjlib/release.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenObject Library
+#    Copyright (C) 2009 Syleam (<http://syleam.fr>). Christophe Chauvet 
+#                  All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+"""Release information"""
+
+name = "OpenObject-Library"
+appname = name.lower()
+version = "1.0dev"
+copyright = "Copyright (C) 2009 Christophe CHAUVET <christophe.chauvet@syleam.fr>"
+description = "Web Services library OpenObject Framework"
+author = "Christophe CHAUVET"
+author_email = "christophe.chauvet@syleam.fr"
+url = "https://launchpad.net/openobject-library"
+long_description = """OpenObject-Library is a library to manage 
+OpenObject Server
+"""
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ setup.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenObject Library
+#    Copyright (C) 2009 Syleam (<http://syleam.fr>). Christophe Chauvet 
+#                  All Rights Reserved
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from distutils.core import setup
+from oobjlib import release
+
+import os
+
+def find_packages(base):
+    """Find all package for this application
+
+    :param base: root path of source.
+    :return: Returns a list of subpackages suitable for setup() packages keyword.
+    """
+    ret = [base]
+    for path in os.listdir(base):
+        if path.startswith('.'):
+            continue
+        full_path = os.path.join(base, path)
+        if os.path.isdir(full_path):
+            ret += find_packages(full_path)
+    return ret
+
+setup(
+    name=release.appname,
+    version=release.version,
+    description=release.description,
+    long_description=release.long_description,
+    author=release.author,
+    author_email=release.author_email,
+    license='GPLv3',
+    url=release.url,
+    packages=find_packages('oobjlib'))
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
