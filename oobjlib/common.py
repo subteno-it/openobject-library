d069f96a7de65647b83294bba4f402534e6fd8b1
Christophe Chauvet <christophe.chauvet@syleam.fr>
2010-04-28 22:40:15 +0200

    [ADD] Add a UniqueList based on List

--- oobjlib/common.py
+++ oobjlib/common.py
@@ -54,4 +54,14 @@ def GetParser(appname, version):
 
     return parser
 
+class UniqueList(list):
+    """Inhherit list to be unique"""
+    def append(self, o):
+        if o and o not in self:
+            super(unique_list, self).append(o)
+
+    def insert(self, p, o):
+        if o and o not in self:
+            super(unique_list, self).insert(p, o)
+
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
