c041e3dad1fe98eaa603d782345fbf4bedbfc741
Christophe Chauvet <christophe.chauvet@syleam.fr>
2010-11-26 10:25:29 +0100

    [FIX] Old syntax for raise an exception, use the new format [IMP] PEP8 Compliant

--- oobjlib/convert.py
+++ oobjlib/convert.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    oobjlib module for OpenERP, 
-#    Copyright (C) 2010 SYLEAM Info Services (<http://www.syleam.fr/>) 
-#              Christophe CHAUVET
+#    oobjlib module for OpenERP,
+#    Copyright (C) 2010 SYLEAM Info Services (<http://www.syleam.fr/>)
+#              Christophe CHAUVET <christophe.chauvet@syleam.fr>
 #
 #    This file is a part of oobjlib
 #
@@ -29,30 +29,32 @@ Global convertion function
 import time
 import locale
 
+
 def Format_Date(datum, date_style):
     """
     Convert date in input to an OpenObject format
 
-    @type  datum: str
-    @param datum: string representation of the date in input
-    @type  date_style: str
-    @param date_style: date format in input
-    @rtype: str
-    @return: date convert for OpenObject
+    :param datum: string representation of the date in input
+    :type  datum: str
+    :param date_style: date format in input
+    :type  date_style: str
+    :return: date convert for OpenObject
+    :rtype: str
     """
     try:
         return time.strftime('%Y-%m-%d', time.strptime(datum, date_style))
     except ValueError:
         return False
 
+
 def Locale_Float(number, loc='en_US'):
     """
     Convert the locale number to a float
 
-    @type  number: str
-    @param number: Number representation in locale
-    @type  loc: str
-    @param loc: Customer locale
+    :param number: Number representation in locale
+    :type  number: str
+    :param loc: Customer locale
+    :type  loc: str
     """
     # Complete locale with encoding if necessary
     if loc.find('.') == -1:
@@ -60,31 +62,33 @@ def Locale_Float(number, loc='en_US'):
     try:
         locale.setlocale(locale.LC_ALL, loc)
     except locale.Error:
-        raise Exception, 'Unsuported locale (%s)' % loc
+        raise Exception('Unsuported locale (%s)' % loc)
     return  locale.atof(number)
+
 
 def Locale_Date(datum, loc='en_US'):
     """
     Convert the locale number to a float
 
-    @type  number: str
-    @param number: Date representation in locale
-    @type  loc: str
-    @param loc: Customer locale
+    :type  number: str
+    :param number: Date representation in locale
+    :type  loc: str
+    :param loc: Customer locale
     """
     # Complete locale with encoding if necessary
     if loc.find('.') == -1:
         loc += '.utf-8'
     try:
-         locale.setlocale(locale.LC_ALL, loc)
+        locale.setlocale(locale.LC_ALL, loc)
     except locale.Error:
-        raise Exception, 'Unsuported locale (%s)' % loc
+        raise Exception('Unsuported locale (%s)' % loc)
     return Format_Date(datum, locale.nl_langinfo(locale.D_FMT))
+
 
 def test():
     try:
         # Convert french date to openobject format
-        assert Format_Date('03/02/2010','%d/%m/%Y') == '2010-02-03'
+        assert Format_Date('03/02/2010', '%d/%m/%Y') == '2010-02-03'
         # Convert french float to a python float
         assert Locale_Float('5,5', 'fr_FR') == 5.5
         # Convert number with thousand separator to a python float
